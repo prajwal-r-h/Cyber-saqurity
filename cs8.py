8b40d386117b229aa821b7a2e9a6e05e0da22a44
Prajwal <119872631+prajwal-r-h@users.noreply.github.com>
2024-01-27 20:35:00 +0530

    Update cs8.py

--- cs8.py
+++ cs8.py
@@ -2,22 +2,10 @@
 
 from cryptography.fernet import Fernet
 key = Fernet.generate_key()
-# Create a Fernet object with the generated key
 f = Fernet(key)
-
-# Take user input for the message to encrypt
-#message = input("Enter the message to encrypt: ")
-
-# Convert the message to bytes and encrypt it
-#token = f.encrypt(message.encode('utf-8'))
-token = f.encrypt(b"klejtbca")
-
-# Print the encrypted token
-print("Encrypted Token:", token)
-
-# Decrypt the token and print the original message
-decrypted_message = f.decrypt(token).decode('utf-8')
-print("Decrypted Message:", decrypted_message)
-
+token = f.encrypt(b"KLE BCA GADAG")
+print(token)
+d = f.decrypt(token)
+print(d)
 
 
